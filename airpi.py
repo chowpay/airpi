5bf0065b79a887a03de0e88bbb058b28e550bf23
chowpay <chowpay@gmail.com>
2020-11-11 23:57:53 -0800

    remoted prints added --d to display debugging

--- airpi.py
+++ airpi.py
@@ -1,14 +1,34 @@
-#Kaho Chan
+#aho Chan
 #Modified script from https://www.raspberrypi.org/blog/monitor-air-quality-with-a-raspberry-pi/
 #11/03/2020
 
+import os
 import sys
-print(sys.path)
 sys.path.append("/home/pi/scripts/airpi/venv38/lib/python3.8/site-packages")
 import yaml
 import serial
 import time
+import argparse
 from Adafruit_IO import Client
+
+
+#flag parser configs
+prog_name = os.path.basename(__file__)
+usage = "\n"+"Example:"+"\n"+"python "+prog_name+" --a"
+parser = argparse.ArgumentParser(description=usage)
+parser.add_argument("--d", action = 'store_true' , help='displays debugging code on screen ie --d')
+args = parser.parse_args()
+
+
+#Disable display on screen for debugging
+def display(msg):
+    if args.d:
+        print (msg)
+    else:
+        pass
+
+
+display(sys.path)
 
 #Load yaml configuration file
 try:
@@ -40,6 +60,6 @@ while True:
     aio.send('kingswoodtwofive', pmtwofive)
     pmten = int.from_bytes(b''.join(data[4:6]),byteorder = 'little') / 10
     aio.send('kingswoodten', pmten)
-    print('PM2.5 = {0}\nPM10 = {1}'.format(pmtwofive,pmten))
+    display('PM2.5 = {0}\nPM10 = {1}'.format(pmtwofive,pmten))
     time.sleep(10)
 
